ca764d83e3f7b935fdd06ca7b410afabc9b2a24d
Laminne Tatsuto Yamamoto <laminne33569@approvers.dev>
2020-08-12 17:28:29 +0900

    Create meigen.py

---
+++ cgi-bin/meigen.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+import sqlite3
+conn = sqlite3.connect('/mnt/c/Users/mtatt/Documents/GitHub/CarryBag/meigen.db')
+c = conn.cursor()
+
+r = c.fetchall()
+print("Content-Type: text/html\n")
+c.execute('SELECT * FROM meigen')
+for row in c.fetchall():
+    print(row)
